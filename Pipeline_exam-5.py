ea20c09ea67f63b9bbdab2d95c0bcc1df089f383
Pernille Ryder <pr.nikolajsen@hotmail.com>
2020-11-18 22:03:32 +0100

    muligt loop sentence segmentation

---
+++ Pipeline_exam-5.py
@@ -0,0 +1,252 @@
+# %%
+# IMPORT PACKAGES
+import lemmy.pipe
+import regex
+import logging
+import morfessor
+from polyglot.text import Text
+from nltk.tokenize import word_tokenize
+from nltk.tokenize import sent_tokenize
+import re
+from collections import Counter
+from functools import reduce
+from operator import add
+import spacy
+import nltk
+import lemmy
+import stanza
+import pandas as pd
+import sys
+import glob
+import os.path
+
+""" ALT DETTE KAN FAKTISK SLETTES
+# %%
+# CLEANING DATA - VIRKER
+
+# Import data
+# regex to remove citations/references and quotes
+import re
+
+with open('Data/D_data/Test.txt', encoding='utf8', errors='ignore') as f:
+    txt = re.sub(
+        r"\(\D*\d?\d{4}(?:, s.? [0-9]+.?.?[0-9].?)?(([;])\D*\d{4})*\)|\(([a-zA-Z]+\d\D*\d{4}\))", "", f.read())
+    txt = re.sub(r'"[^"]+"', "", txt)
+    contents = f.read()
+    print(txt)
+
+
+# %%
+# VIRKER TIL AT GEMME NY FIL
+# DER SKAL LAVES ET FOR LOOP FOR AT KLARE ALLE FILER
+out = open('Data/D_data/Testfile.txt', 'w')
+out.write(txt)
+out.close()
+"""
+
+# %%
+# HENTER DATA IND --> CLEANER MED REGEX --> GEMMER I NY MAPPE "Final_D_data" OG "Final_ND_data"
+# Cleaning Dyslexia data VIRKER
+
+list_of_files = glob.glob("Data/D_data/*.txt")
+
+for file_name in list_of_files:
+    print(file_name)  # Dette kan kommenteres ud hvis vi har lyst
+
+    # This needs to be done *inside the loop*
+    f = open(file_name, "r", encoding="utf8", errors="ignore")
+    lst = []
+    for line in f:
+        line.strip()
+        line = re.sub(
+            r"\(\D*\d?\d{4}(?:, s.? [0-9]+.?.?[0-9].?)?(([;])\D*\d{4})*\)|\(([a-zA-Z]+\d\D*\d{4}\))",
+            "",
+            f.read(),
+        )
+        line = re.sub(r'”[^"]+”', "", line)
+        line = re.sub(r'"[^"]+"', "", line)
+        lst.append(line)
+    f.close()
+
+    f = open(os.path.join("Data/D_data/Final_D_data", os.path.basename(file_name)), "w")
+
+    for line in lst:
+        f.write(line)
+    f.close()
+
+# %%
+# Cleaning Non-Dyslexia data VIRKER
+
+list_of_files = glob.glob("Data/ND_data/*.txt")
+
+for file_name in list_of_files:
+    print(file_name)  # Dette kan kommenteres ud hvis vi har lyst
+
+    # This needs to be done *inside the loop*
+    f = open(file_name, "r", encoding="utf8", errors="ignore")
+    lst = []
+    for line in f:
+        line.strip()
+        line = re.sub(
+            r"\(\D*\d?\d{4}(?:, s.? [0-9]+.?.?[0-9].?)?(([;])\D*\d{4})*\)|\(([a-zA-Z]+\d\D*\d{4}\))",
+            "",
+            f.read(),
+        )
+        line = re.sub(r'”[^"]+”', "", line)
+        line = re.sub(r'"[^"]+"', "", line)
+        lst.append(line)
+    f.close()
+
+    f = open(
+        os.path.join("Data/ND_data/Final_ND_data", os.path.basename(file_name)), "w"
+    )
+
+    for line in lst:
+        f.write(line)
+    f.close()
+
+
+# %% Prøver segmentation med for loop VIKER IKKE
+path = glob.glob("Data/D_data/Testfolder/*.txt")
+print(path)
+
+
+def sentence_segmentation(path):
+    for file_name in path:
+        f = open(file_name, "r", encoding="utf8", errors="ignore")
+
+    for words in path:
+        segment = sent_tokenize(f)
+
+    return segment
+
+    print(segment)
+
+
+# %%
+# SENTENCE SEGMENTATION - VIRKER
+
+print(sent_tokenize(txt))
+
+# Det nye regex fra Mikkel: = ([.?!)(?![\s]*[\d])
+
+# %%
+# TOKENIZATION - VIRKER
+
+tokens = nltk.tokenize.word_tokenize(txt)
+print(tokens)
+
+# %%
+# TOKEN FREQUENCIES - VIRKER
+freq = Counter(tokens)
+freq
+freq.most_common
+
+# %%
+# Lemmatization VIRKER
+l = lemmy.load("da")
+
+
+def lemmatize_sentence(sentence, lemmatizer=l):
+    return [lemmatizer.lemmatize("", word) for word in sentence]
+
+
+lemmas = lemmatize_sentence(txt.split())
+print(lemmas)
+
+# %%
+
+nlp = stanza.Pipeline(lang="da", processors="tokenize,pos,lemma")
+doc = nlp(tokens)
+print(
+    *[
+        f'word: {word.text+" "}\tlemma: {word.lemma}'
+        for sent in doc.sentences
+        for word in sent.words
+    ],
+    sep="\n",
+)
+
+# %%
+
+# %%
+# nlp = spacy.load("da_core_news_sm")
+spacy.load("da_core_news_sm")
+
+
+# %%
+# SpaCy POS tag - VIRKER hvis lemma gør
+nlp = spacy.load("da_core_news_sm")
+doc = nlp(txt)
+
+for token in doc:
+    print(token.text, lemmas, token.pos_, token.is_stop)
+
+# %%
+# VIRKER, MEN ER DÅRLIG
+
+blob = txt
+
+text = Text(blob)
+
+text.pos_tags
+
+# %%
+tokenlist = tokens
+
+# %%
+
+
+def postag_stanza(tokenlist):
+
+    # pass
+
+    nlp = stanza.Pipeline(
+        lang="da", processors="pos,tokenize,lemma", tokenize_pretokenized=True
+    )
+    doc = nlp(tokenlist)
+
+    """res = [
+        (word.postag_stanza)
+        for n_sent, sent in enumerate(doc.sentences)
+        for word in sent.words
+    ]
+"""
+    # if return_df:
+
+    # return pd.DataFrame(res)
+    # return res
+
+
+# %%
+nlp = postag_stanza(tokenlist=tokenlist)
+print(nlp)
+
+# %%
+
+
+def lemmatize_stanza(txt):
+    nlp = stanza.Pipeline(lang="da", processors="tokenize,mwt,pos,lemma")
+    doc = nlp(txt)
+    print(
+        *[
+            f'word: {word.text+" "}\tlemma: {word.lemma}\tPOS: {word.POS}'
+            for sent in doc.sentences
+            for word in sent.words
+        ],
+        sep="\n",
+    )
+
+
+# %%
+print(word.lemmas)
+
+
+# %%
+lemmatizer = lemmy.load("da")
+lemmatizer.lemmatize("", "elsker")
+# %%
+
+logging.basicConfig(format="%(levelname)s : %(message)s", level=logging.DEBUG)
+# %%
+nlp = da.load()
