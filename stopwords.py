4f49910c123aa44b17dfc16cf25ecd8835a981dd
RikkeKjems <31722239+RikkeKjems@users.noreply.github.com>
2020-11-21 14:28:26 +0100

    Stopwords virker stadig ikke

--- stopwords.py
+++ stopwords.py
@@ -38,12 +38,13 @@ print(stop_words)
 # %% VIRKER PÅ 1 FIL
 # word_tokenize accepts a string as an input, not a file.
 stop_words = set(stopwords.words('danish'))
-file1 = open("Testtext.txt")
+file1 = open("Data/UTF8testfolder/Testtext.txt",
+             "r", encoding="utf8", errors="ignore")
 line = file1.read()  # Use this to read file content as a stream:
 words = line.split()
 for r in words:
     if not r in stop_words:
-        appendFile = open('filteredtext.txt', 'a')
+        appendFile = open('Data/UTF8testfolder/filteredtext.txt', 'a')
         appendFile.write(" "+r)
         appendFile.close()
 
@@ -77,10 +78,9 @@ for file_name in path:
         contents = f.read()  # læs indholdet i filen
         # print(contents)  #print indholdet - Kan undlades, tjekker om vi er inde i filen
 
-    for r in file_name:
-        if not r in stop_words:
-            seg_lst.append(" " + r)
+    for words in file_name:
+        segment = [if word in stop_words else word for word in words]
+        # gem segmentation for hver dokument i en liste
+        seg_lst.append(segment)
 
-print(seg_lst)
-
-# %%
+print(seg_lst)  # print liste
