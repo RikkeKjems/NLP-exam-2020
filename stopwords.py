04b4f4a605b3a72bc9c52012d18e9f751fc3c5f9
RikkeKjems <31722239+RikkeKjems@users.noreply.github.com>
2020-11-22 10:31:43 +0100

    new data

--- stopwords.py
+++ stopwords.py
@@ -84,3 +84,29 @@ for file_name in path:
         seg_lst.append(segment)
 
 print(seg_lst)  # print liste
+
+# %%
+list_of_files = glob.glob("Data/ND22.txt")
+
+for file_name in list_of_files:
+    print(file_name)  # Dette kan kommenteres ud hvis vi har lyst
+
+    # This needs to be done *inside the loop*
+    f = open(file_name, "r", encoding="utf8", errors="ignore")
+    lst = []
+    for line in f:
+        line.strip()
+        line = re.sub(
+            r"\(\D*\d?\d{4}(?:, s.? [0-9]+.?.?[0-9].?)?(([;])\D*\d{4})*\)|\(([a-zA-Z]+\d\D*\d{4}\))",
+            "",
+            f.read(),
+        )
+        lst.append(line)
+    f.close()
+
+    f = open(os.path.join("Data", os.path.basename(file_name)), "w")
+
+    for line in lst:
+        f.write(line)
+    f.close()
+# %%
