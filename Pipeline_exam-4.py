6443b46af0165a0749d4733b9d2c5eb56b4e0822
Pernille Ryder <pr.nikolajsen@hotmail.com>
2020-11-24 11:27:32 +0100

    ren pipeline

--- Pipeline_exam-4.py
+++ Pipeline_exam-4.py
@@ -95,6 +95,8 @@ for file_name in path:
 
 # %% STOPWORDS VIRKER
 import nltk
+
+nltk.download("stopwords")
 from nltk.corpus import stopwords
 
 stop = set(stopwords.words("danish"))
@@ -109,132 +111,20 @@ for t in tokens:
 print(stop_lst)
 
 
-#%%
+#%% STOP WORDS FRA SPACY ER DE BEDRE?
 # 219 STOP WORDS
 len(STOP_WORDS)
 print(STOP_WORDS)
 
-# %% VIRKER
-# 94 STOP WORDS
-nltk.download("stopwords")
-
-words = stopwords.words("danish")
-len(words)
-print(words)
 # %%
-# TOKEN FREQUENCIES - VIRKER IKKE
-freq = Counter(token_lst)
-freq
-freq.most_common
-# %%
-tokenfreq = token_lst.count
-len(tokenfreq)
-print(tokenfreq)
-# %%
-# Lemmatization VIRKER
-l = lemmy.load("da")
+# TOKEN FREQUENCIES - VIRKER
+freq = Counter(stop_lst)
 
+tf = freq.most_common
+print(tf)
+# %% FREQ LIST
+import pandas as pd
+from pandas import DataFrame
 
-def lemmatize_sentence(sentence, lemmatizer=l):
-    return [lemmatizer.lemmatize("", word) for word in sentence]
-
-
-lemmas = lemmatize_sentence(txt.split())
-print(lemmas)
-
-# %%
-
-nlp = stanza.Pipeline(lang="da", processors="tokenize,pos,lemma")
-doc = nlp(tokens)
-print(
-    *[
-        f'word: {word.text+" "}\tlemma: {word.lemma}'
-        for sent in doc.sentences
-        for word in sent.words
-    ],
-    sep="\n",
-)
-
-# %%
-
-# %%
-# nlp = spacy.load("da_core_news_sm")
-spacy.load("da_core_news_sm")
-
-
-# %%
-# SpaCy POS tag - VIRKER hvis lemma gør
-nlp = spacy.load("da_core_news_sm")
-doc = nlp(txt)
-
-for token in doc:
-    print(token.text, lemmas, token.pos_, token.is_stop)
-
-# %%
-# VIRKER, MEN ER DÅRLIG
-
-blob = txt
-
-text = Text(blob)
-
-text.pos_tags
-
-# %%
-tokenlist = tokens
-
-# %%
-
-
-def postag_stanza(tokenlist):
-
-    # pass
-
-    nlp = stanza.Pipeline(
-        lang="da", processors="pos,tokenize,lemma", tokenize_pretokenized=True
-    )
-    doc = nlp(tokenlist)
-
-    """res = [
-        (word.postag_stanza)
-        for n_sent, sent in enumerate(doc.sentences)
-        for word in sent.words
-    ]
-"""
-    # if return_df:
-
-    # return pd.DataFrame(res)
-    # return res
-
-
-# %%
-nlp = postag_stanza(tokenlist=tokenlist)
-print(nlp)
-
-# %%
-
-
-def lemmatize_stanza(txt):
-    nlp = stanza.Pipeline(lang="da", processors="tokenize,mwt,pos,lemma")
-    doc = nlp(txt)
-    print(
-        *[
-            f'word: {word.text+" "}\tlemma: {word.lemma}\tPOS: {word.POS}'
-            for sent in doc.sentences
-            for word in sent.words
-        ],
-        sep="\n",
-    )
-
-
-# %%
-print(word.lemmas)
-
-
-# %%
-lemmatizer = lemmy.load("da")
-lemmatizer.lemmatize("", "elsker")
-# %%
-
-logging.basicConfig(format="%(levelname)s : %(message)s", level=logging.DEBUG)
-# %%
-nlp = da.load()
+df = pd.DataFrame(tf, columns=["tf"])
+df
--- stopwords2.py
+++ stopwords2.py
@@ -26,10 +26,11 @@ from polyglot.text import Text
 # %%
 import nltk
 from nltk.corpus import stopwords
-print(stopwords.words('danish'))
+
+print(stopwords.words("danish"))
 
 # %%
-len(stopwords.words('danish'))
+len(stopwords.words("danish"))
 print(stop_words)
 # %%
 # NLTK STOPWORDS = 94
@@ -37,15 +38,14 @@ print(stop_words)
 
 # %% VIRKER PÅ 1 FIL
 # word_tokenize accepts a string as an input, not a file.
-stop_words = set(stopwords.words('danish'))
-file1 = open("Data/UTF8testfolder/Testtext.txt",
-             "r", encoding="utf8", errors="ignore")
+stop_words = set(stopwords.words("danish"))
+file1 = open("Data/UTF8testfolder/Testtext.txt", "r", encoding="utf8", errors="ignore")
 line = file1.read()  # Use this to read file content as a stream:
 words = line.split()
 for r in words:
     if not r in stop_words:
-        appendFile = open('Data/UTF8testfolder/filteredtext.txt', 'a')
-        appendFile.write(" "+r)
+        appendFile = open("Data/UTF8testfolder/filteredtext.txt", "a")
+        appendFile.write(" " + r)
         appendFile.close()
 
 
@@ -53,23 +53,33 @@ for r in words:
 # VIRKER OVERHOVEDET IKKE
 import nltk
 from nltk.corpus import stopwords
-print(stopwords.words('danish'))
+
+# print(stopwords.words('danish'))
 
 path = glob.glob("Data/UTF8testfolder/*.txt")
-stop_words = set(stopwords.words('danish'))
+stop_words = set(stopwords.words("danish"))
+# print (stop_words)
+
+txt = "Jeg er den bedste af de bedste. Igår gik vi en tur"
 
 for file_name in path:
     f = open(file_name, "r", encoding="utf8", errors="ignore")
-    seg_lst = []  # tom liste
 
     if f.mode == "r":  # tjek om filen kan læses
         contents = f.read()  # læs indholdet i filen
-        #print(contents)  #print indholdet - Kan undlades, tjekker om vi er inde i filen
+        # print(contents)  #print indholdet - Kan undlades, tjekker om vi er inde i filen
 
-    for words in contents:
-        #segment = [if word in stop_words else word for word in words]
-        segment = [word for word in contents if word not in stopwords.words('danish')]
-        # gem segmentation for hver dokument i en liste
-        seg_lst.append(segment)
+    stop_lst = []  # tom liste
+    # for words in contents:
+    # segment = [if word in stop_words else word for word in words]
+    # segment = [for word in contents if word not in stop_words]
+for c in txt:
+    if c not in stop_words:
+        stop_lst.append(c)
 
-print(seg_lst)  # print liste
+print(stop_lst)  # print liste
+
+
+def lemmatize_sentence(sentence, lemmatizer=l):
+    return [lemmatizer.lemmatize("", word) for word in sentence]
+
