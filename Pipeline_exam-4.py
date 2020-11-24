8e45f55313077620da2f9bded253070b65ffa1c3
Pernille Ryder <pr.nikolajsen@hotmail.com>
2020-11-24 22:05:04 +0100

    freq virker ikke

--- Pipeline_exam-4.py
+++ Pipeline_exam-4.py
@@ -110,7 +110,7 @@ for file_name in path:
     token_texts.close()
     i += 1
 
-# print(token_lst)  # print liste
+
 
 # FOR ND DATA
 # %%
@@ -145,7 +145,7 @@ for file_name in path:
     token_texts.close()
     i += 1
 
-# print(token_lst)  # print liste
+
 
 # %%
 # TOKEN FREQUENCIES - VIRKER
@@ -153,6 +153,93 @@ freq = Counter(without_stop_lst)
 
 tf = freq.most_common
 print(tf)
+
+
+import collections
+
+c = collections.Counter()
+data = ('Data/Final_UTF8_data/ND_Data/ND_Tokenfolder', 'rt')
+for file in data:
+    f = open(file_name, "r", encoding="utf8", errors="ignore")
+    for line in f:
+        c.update(line.rstrip().lower())
+
+d = {}
+
+for token in f: 
+    d[token] = d.get(token, 0) + 1
+
+word_freq = []
+for key, value in d.items():
+    word_freq.append((value, key))
+
+word_freq.sort(reverse=True) 
+print(word_freq)
+
+
+
+print=('Most common:')
+    for token, count in c.most_common(3):
+        print '%s: %7d' % (token, count)
+
+from collections import Counter
+Counter(word_list).most_common()
+
+
+############################
+data = ('Data/Final_UTF8_data/ND_Data/ND_Tokenfolder', 'rt')
+for file_name in data:
+    f = open(file_name, "r", encoding="utf8", errors="ignore")
+    d = []  # tom liste
+    if f.mode == "r":  # tjek om filen kan læses
+        contents = f.read()  # læs indholdet i filen
+        # print(contents)  #print indholdet - Kan undlades, tjekker om vi er inde i filen
+
+
+
+
+# counting number of times each word comes up in list of words
+for key in f: 
+    d[key] = d.get(key, 0) + 1
+
+sorted(d.items(), key = lambda x: x[1], reverse = True)
+
+#%%
+import os
+import re
+from os.path import join
+from collections import Counter, OrderedDict
+
+directory = ('Data/Final_UTF8_data/ND_Data/ND_Tokenfolder', 'rt')
+def count_words(directory): # don't use the name dir, it's a builtin function
+    """Counts word frequencies in a directory of files.
+
+    Keyword arguments:
+    directory -- count_words will search this directory recursively
+    ext -- the extension of files that you wish to count
+
+    Returns an OrderedDict, from most to least frequent.
+    """
+
+    # Initialize the counter
+    word_counter = Counter()
+
+    # Personally I like to break my code into small, simple functions
+    # This code could be inline in the loop below,
+    # but I think it's a bit clearer this way.
+    def update_counter(word_counter, filename):
+        '''updates word_counter with all the words in open(filename)'''
+        with open(filename, 'r') as f:
+
+    # Using os.walk instead of glob
+            for root, dirs, files in os.walk(".txt", topdown=True):
+                for fname in files:
+                    if fname.endswith(xt):
+                        update_counter(word_counter, join(root, fname))
+            
+
+    # words_counter.most_common() does exactly the sort you are looking for
+        return word_counter.most_common()
 # %% FREQ LIST
 
 df = pd.DataFrame(tf, columns=["tf"])
