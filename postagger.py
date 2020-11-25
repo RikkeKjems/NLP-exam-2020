df6dc2fe9f827e1a2d78e5b44325ff81360a7ef8
RikkeKjems <31722239+RikkeKjems@users.noreply.github.com>
2020-11-25 13:39:07 +0100

    ækaf

--- postagger.py
+++ postagger.py
@@ -1,5 +1,6 @@
 # %%
 # IMPORT PACKAGES
+import collections
 from stanza.pipeline.processor import ProcessorVariant, register_processor_variant
 from nltk.corpus import stopwords
 from spacy.lang.da.stop_words import STOP_WORDS
@@ -51,7 +52,7 @@ def postagger(text, stanza_pipeline):
 
 # %% VIRKER!!! SKAL RETTES TIL RIGTIGE FOLDER
 # FOR D_DATA
-path = glob.glob("Data/Testfolder/Tokenfolder/*.txt")
+path = glob.glob("Data/Final_UTF8_data/D_data/D_Tokenfolder/*.txt")
 i = 0
 for file_name in path:
     f = open(file_name, "r", encoding="utf8", errors="ignore")
@@ -70,7 +71,7 @@ for file_name in path:
         newFile = "D" + str(i)  # kan ændres hvis vi vil have D og ND
         print(newFile)
         tagged_texts = open(
-            f'Data/Testfolder/Tokenfolder/tagged_{newFile}.txt', 'w')
+            f'Data/Final_UTF8_data/D_postagged/tagged_{newFile}.txt', 'w')
         for tagged in pos_tagged:
             tagged_texts.write(str(tagged))
         tagged_texts.close()
@@ -159,4 +160,63 @@ for file_name in path:
         for tagged in pos_tagged:
             tagged_texts.write(str(tagged))
 
+        i += 1
+
+
+# %%
+dico = {}
+# just to init the dict and avoid checking if index exist...
+for i in range(1, 31):
+    dico[i] = 0
+
+with open("Data/Testfolder/D1 copy.txt", encoding="utf-8") as f:  # better to use in that way
+    line = f.read()
+    for word in line.split(" "):
+        dico[len(word)] += 1
+
+print(dico)
+# %%
+
+
+def main():
+    counts = collections.defaultdict(int)
+    with open('Data/Testfolder/ND4 copy.txt', 'rt', encoding='utf-8') as file:
+        for word in file.read().split():
+            counts[len(word)] += 1
+    print('length | How often a word of this length occurs')
+    for i in sorted(counts.keys()):
+        print('%-6d | %d' % (i, counts[i]))
+
+
+if __name__ == '__main__':
+    main()
+
+
+# VI PRØVER LIGE MED DET HER
+# %%
+path = glob.glob("Data/Final_UTF8_data/D_data/D_Tokenfolder/*.txt")
+i = 0
+for file_name in path:
+    f = open(file_name, "r", encoding="utf8", errors="ignore")
+    if f.mode == "r":  # tjek om filen kan læses
+        contents = f.read()  # læs indholdet i filen
+        texts = contents.split('/')
+        texts.sort()
+        out = []
+        for text in texts:
+            new = text.replace("[", "")
+            new = new.replace("]", "")
+            new = new.replace("'", "")
+            if (new != ""):
+                out.append(new)
+        print(out)
+
+        pos_tagged = [postagger(text, s_nlp) for text in out]
+        newFile = "D" + str(i)  # kan ændres hvis vi vil have D og ND
+        print(newFile)
+        tagged_texts = open(
+            f'Data/Testfolder/Tokenfolder/tagged/tagged_{newFile}.txt', 'w')
+        for tagged in pos_tagged:
+            tagged_texts.write(str(tagged))
+        tagged_texts.close()
         i += 1
