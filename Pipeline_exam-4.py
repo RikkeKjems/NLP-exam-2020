83b78d6aa8a31caceb76b6d86b63c01e6b0a2422
RikkeKjems <31722239+RikkeKjems@users.noreply.github.com>
2020-11-26 20:01:49 +0100

    Rettelser

--- Pipeline_exam-4.py
+++ Pipeline_exam-4.py
@@ -1,5 +1,6 @@
 # %%
 # IMPORT PACKAGES
+from string import digits
 from os import path
 from pandas import DataFrame
 from nltk.corpus import stopwords
@@ -24,6 +25,7 @@ import lemmy.pipe
 import morfessor
 from polyglot.text import Text
 
+""" # ALT DET HER SKAL VI IKKE KØRE MERE. DET ER KØRT OG DER ER DANNET FILER
 # %% CLEANING
 # HENTER AL DATA IND --> CLEANER MED REGEX --> GEMMER I NY MAPPE "Final_Data"
 # Cleaning AL data VIRKER
@@ -76,11 +78,11 @@ for file_name in path:
 # %%
 # DET HER SKAL VI VEL HAVE IND ET STED?
 # Det nye regex fra Mikkel: = ([.?!)(?![\s]*[\d])
+"""
 
-# FOR D DATA
+# RIKKE ARBEJDER HER
+# NEW TOKENIZATION FUNCTION
 # %%
-# Loop Tokenization - VIRKER
-# Filer hentes fra Final_UTF8_data da alle filer skal være uft8
 path = glob.glob("Data/Final_UTF8_data/D_data/*.txt")
 i = 1
 stop = set(stopwords.words("danish"))
@@ -97,57 +99,82 @@ for file_name in path:
     print(len(tokens))
     for token in tokens:
         if token not in stop:
-            token_lst.append(nltk.tokenize.word_tokenize(token))
+            if token not in string.punctuation:
+                if token not in string.digits:
+                    token_lst.append(nltk.tokenize.word_tokenize(token))
     print(len(token_lst))
+
+    #token_lst = list(filter(lambda token: token not in string.punctuation, token_lst))
 
     newFile = "D_token" + str(i)  # kan ændres hvis vi vil have D og ND
     print(newFile)
-    token_texts = open(f"Data/Final_UTF8_data/D_Data/D_Tokenfolder/{newFile}.txt", "w")
-    for token in token_lst:
-        token_texts.write(str(token))
-        token_texts.write("/")
-    token_texts.close()
-    i += 1
-
-
-# FOR ND DATA
-# %%
-# Loop Tokenization - VIRKER
-# Stopwords removal
-# Filer hentes fra Final_UTF8_data da alle filer skal være uft8
-path = glob.glob("Data/Final_UTF8_data/ND_data/*.txt")
-i = 1
-stop = set(stopwords.words("danish"))
-
-for file_name in path:
-    f = open(file_name, "r", encoding="utf8", errors="ignore")
-    token_lst = []  # tom liste
-    if f.mode == "r":  # tjek om filen kan læses
-        contents = f.read()  # læs indholdet i filen
-        # print(contents)  #print indholdet - Kan undlades, tjekker om vi er inde i filen
-
-    tokens = nltk.tokenize.word_tokenize(contents)  # tokenization function
-    # gem segmentation for hvert dokument i en liste
-    print(len(tokens))
-    for token in tokens:
-        if token not in stop:
-            token_lst.append(nltk.tokenize.word_tokenize(token))
-    print(len(token_lst))
-
-    newFile = "ND_token" + str(i)  # kan ændres hvis vi vil have D og ND
-    print(newFile)
     token_texts = open(
-        f"Data/Final_UTF8_data/ND_Data/ND_Tokenfolder/{newFile}.txt", "w"
+        f"Data/Final_UTF8_data/D_Data/D_Tokenfolder/{newFile}.txt", "w"
     )
     for token in token_lst:
         token_texts.write(str(token))
         token_texts.write("/")
     token_texts.close()
     i += 1
-#%%
-###    WORD FREQ - Pernille arbejder her
 
-## VIRKER
+# %%
+# RIKKE ARBEJDER OGSÅ LIDT HER
+# POSTAGGING
+stanza.download('da')
+
+# %%VIRKER
+s_nlp = stanza.Pipeline(lang='da',
+                        processors='tokenize,pos,lemma',
+                        use_gpu=False)
+
+# %%VIRKER
+
+
+def postagger(text, stanza_pipeline):
+    """
+    Return lemmas as generator
+    """
+    doc = stanza_pipeline(text)
+    postag = [(word.lemma, word.upos)
+              for sent in doc.sentences
+              for word in sent.words]
+    return postag
+
+
+# %%
+path = glob.glob("Data/Final_UTF8_data/D_data/D_Tokenfolder/*.txt")
+i = 0
+for file_name in path:
+    f = open(file_name, "r", encoding="utf8", errors="ignore")
+    if f.mode == "r":  # tjek om filen kan læses
+        contents = f.read()  # læs indholdet i filen
+        texts = contents.split('/')
+        texts.sort()
+        out = []
+        for text in texts:
+            new = text.replace("[", "")
+            new = new.replace("]", "")
+            new = new.replace("'", "")
+            if (new != ""):
+                out.append(new)
+        # print(out)
+
+        pos_tagged = [postagger(text, s_nlp) for text in out]
+        newFile = "D" + str(i)  # kan ændres hvis vi vil have D og ND
+        print(newFile)
+        tagged_texts = open(
+            f'Data/Final_UTF8_data/New_D_postagged/tagged_{newFile}.txt', 'w')
+        for tagged in pos_tagged:
+            tagged_texts.write(str(tagged))
+        tagged_texts.close()
+        i += 1
+
+
+# %%
+# WORD FREQ - Pernille arbejder her
+# Den her kører på det der ikke er tokenized eller fjernet stopwords på
+
+# VIRKER
 file = open("Data/Final_UTF8_data/D_Data/D5 copy.txt", "rt")
 data = file.read()
 words = data.split()
@@ -164,13 +191,14 @@ for word in words:
 file.close()
 print(freqs)
 
-### CREATE DF WITH WORDS AND FREQ - VIRKER
+# %%
+# CREATE DF WITH WORDS AND FREQ - VIRKER
 d = freqs
 df = pd.DataFrame(data=d)
 df
 
-#%%
-#### FORSØG PÅ LOOP - VIRKER også på tokenfolder
+# %%
+# FORSØG PÅ LOOP - VIRKER også på tokenfolder
 path = glob.glob("Data/Final_UTF8_data/ND_data/ND_Tokenfolder/*.txt")
 # stop = set(stopwords.words("danish"))
 
@@ -201,9 +229,9 @@ print(
 # Tjek om den lopper gennem alle filer i Tokenfolder
 
 
-#### Lav df med ovenstående info i
+# Lav df med ovenstående info i
 
-### opdel freqs så ord og freq ikke hænger sammen
+# opdel freqs så ord og freq ikke hænger sammen
 # printing iniial_dictionary
 print("intial_dictionary", str(freqs))
 
@@ -219,8 +247,8 @@ print("values : ", str(values))  # values = frequency
 df3 = pd.DataFrame(values, index=keys)
 df3
 
-#%%
-### UNIQUE WORDS - WORD FREQ OF 1 - VIRKER IKKE
+# %%
+# UNIQUE WORDS - WORD FREQ OF 1 - VIRKER IKKE
 
 # extract values=frequency = 1.
 uniques = []  # tom liste
@@ -229,15 +257,17 @@ for keys, values in freqs:  # for ord og frequency i dict freqs
     if values == un:  # hvis frequency er = 1
         print(keys)  # print det ord som har frequency på 1
 
-### kan bruges senere, kommenerede for at VS code ikke bruger det.
+# kan bruges senere, kommenerede for at VS code ikke bruger det.
 # uniques.append(keys) #append til liste
 # print(uniques) #printe liste med 1-taller
 
 
-#%%
+# %%
 
 print(keys)
-### WORD LENGTH - VIRKER IKKE
+# WORD LENGTH - VIRKER IKKE
+
+
 def string_k(k, str):
 
     # create the empty string
@@ -265,54 +295,3 @@ def string_k(k, str):
 k = 8
 str = keys
 print(string_k(k, str))
-
-
-#%%%
-#####
-# POSTAGGING
-
-# %%
-stanza.download("da")
-
-# %%VIRKER
-s_nlp = stanza.Pipeline(lang="da", processors="tokenize,pos,lemma", use_gpu=False)
-
-
-def postagger(text, stanza_pipeline):
-    """
-    Return lemmas as generator
-    """
-    doc = stanza_pipeline(text)
-    postag = [(word.lemma, word.upos) for sent in doc.sentences for word in sent.words]
-    return postag
-
-
-# %% VIRKER!!! SKAL RETTES TIL RIGTIGE FOLDER
-# FOR D_DATA
-path = glob.glob("Data/Testfolder/Tokenfolder/*.txt")
-i = 0
-for file_name in path:
-    f = open(file_name, "r", encoding="utf8", errors="ignore")
-    if f.mode == "r":  # tjek om filen kan læses
-        contents = f.read()  # læs indholdet i filen
-        texts = contents.split("/")
-        texts.sort()
-        out = []
-        for text in texts:
-            new = text.replace("[", "")
-            new = new.replace("]", "")
-            new = new.replace("'", "")
-            if new != "":
-                out.append(new)
-        print(out)
-
-        pos_tagged = [postagger(text, s_nlp) for text in out]
-        newFile = "D" + str(i)  # kan ændres hvis vi vil have D og ND
-        print(newFile)
-        tagged_texts = open(
-            f"Data/Testfolder/Tokenfolder/tagged/tagged_{newFile}.txt", "w"
-        )
-        for tagged in pos_tagged:
-            tagged_texts.write(str(tagged))
-        tagged_texts.close()
-        i += 1
--- postagger.py
+++ postagger.py
@@ -249,3 +249,93 @@ for file_name in path:
         tagged_texts.close()
         i += 1
 # %%
+
+
+# %%%
+#####
+# POSTAGGING
+
+# %%
+stanza.download("da")
+
+# %%VIRKER
+s_nlp = stanza.Pipeline(
+    lang="da", processors="tokenize,pos,lemma", use_gpu=False)
+
+
+def postagger(text, stanza_pipeline):
+    """
+    Return lemmas as generator
+    """
+    doc = stanza_pipeline(text)
+    postag = [(word.lemma, word.upos)
+              for sent in doc.sentences for word in sent.words]
+    return postag
+
+
+# %% VIRKER!!! SKAL RETTES TIL RIGTIGE FOLDER
+# FOR D_DATA
+path = glob.glob("Data/Testfolder/Tokenfolder/*.txt")
+i = 0
+for file_name in path:
+    f = open(file_name, "r", encoding="utf8", errors="ignore")
+    if f.mode == "r":  # tjek om filen kan læses
+        contents = f.read()  # læs indholdet i filen
+        texts = contents.split("/")
+        texts.sort()
+        out = []
+        for text in texts:
+            new = text.replace("[", "")
+            new = new.replace("]", "")
+            new = new.replace("'", "")
+            if new != "":
+                out.append(new)
+        print(out)
+
+        pos_tagged = [postagger(text, s_nlp) for text in out]
+        newFile = "D" + str(i)  # kan ændres hvis vi vil have D og ND
+        print(newFile)
+        tagged_texts = open(
+            f"Data/Testfolder/Tokenfolder/tagged/tagged_{newFile}.txt", "w"
+        )
+        for tagged in pos_tagged:
+            tagged_texts.write(str(tagged))
+        tagged_texts.close()
+        i += 1
+
+
+# NEW TOKENIZATION FUNCTION
+# %%
+path = glob.glob("Data/Final_UTF8_data/D_data/*.txt")
+i = 1
+stop = set(stopwords.words("danish"))
+
+for file_name in path:
+    f = open(file_name, "r", encoding="utf8", errors="ignore")
+    token_lst = []  # tom liste
+    if f.mode == "r":  # tjek om filen kan læses
+        contents = f.read()  # læs indholdet i filen
+        # print(contents)  #print indholdet - Kan undlades, tjekker om vi er inde i filen
+
+    tokens = nltk.tokenize.word_tokenize(contents)  # tokenization function
+    # gem segmentation for hvert dokument i en liste
+    print(len(tokens))
+    for token in tokens:
+        if token not in stop:
+            if token not in string.punctuation:
+                token_lst.append(nltk.tokenize.word_tokenize(token))
+    print(len(token_lst))
+
+    #token_lst = list(filter(lambda token: token not in string.punctuation, token_lst))
+
+    newFile = "D_token" + str(i)  # kan ændres hvis vi vil have D og ND
+    print(newFile)
+    token_texts = open(
+        f"Data/Final_UTF8_data/D_Data/New_D_Tokenfolder/{newFile}.txt", "w"
+    )
+    for token in token_lst:
+        token_texts.write(str(token))
+        token_texts.write("/")
+    token_texts.close()
+    i += 1
+# %%
