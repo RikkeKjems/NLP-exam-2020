45e947fed5829afea2295668219c7ce6a3707504
RikkeKjems <31722239+RikkeKjems@users.noreply.github.com>
2020-12-04 10:29:55 +0100

    infinity loop

--- word_freq.py
+++ word_freq.py
@@ -29,12 +29,12 @@ from polyglot.text import Text
 
 # %%
 # VIRKER men med forkert unique
-## Loop med df som output. filenavn + antal unique words in each file
+# Loop med df som output. filenavn + antal unique words in each file
 
 path = glob.glob('Data/Final_UTF8_data/ND_data/ND_Tokenfolder/*.txt')
 
-idx = [] #filenames for rows = 22
-number =[] #burde også være 22 et tal for hver doc
+idx = []  # filenames for rows = 22
+number = []  # burde også være 22 et tal for hver doc
 for t in path:
     data = open(t, "r").read()
     words = data.split('/')
@@ -47,76 +47,74 @@ for word in words:
         freqs[word] += 1
 
     keys = freqs.keys()  # word
-    values = freqs.values() #frequency
+    values = freqs.values()  # frequency
 
     colm = ['Freq']
     df = pd.DataFrame(data=values, index=keys, columns=colm)
     total_n = (len(df))
     print(total_n)
     df2 = (df.loc[df['Freq'] == 1])
-    num=(len(df2)) # går galt når jeg appender til listen "number"
+    num = (len(df2))  # går galt når jeg appender til listen "number"
     print(num)
-   
+
 c = ['Unique words in doc']
-big_df = pd.DataFrame(data=num, index=idx, columns=c) 
+big_df = pd.DataFrame(data=num, index=idx, columns=c)
 big_df
 
 unique_percentage = num / total_n * 100
 
-#big_df.to_csv(r'Data/Unique_ND.csv')
+# big_df.to_csv(r'Data/Unique_ND.csv')
 
-#%% 
+# %%
 # VIRKER men med forkert unique
-## Loop med df som output. filenavn + antal unique words in each file
+# Loop med df som output. filenavn + antal unique words in each file
 
 path = glob.glob('Data/Final_UTF8_data/D_data/D_Tokenfolder/*.txt')
 
-idx = [] #filenames for rows = 24
-number =[] #burde også være 24 et tal for hver doc
+idx = []  # filenames for rows = 24
+number = []  # burde også være 24 et tal for hver doc
 for t in path:
     data = open(t, "r").read()
     words = data.split('/')
     idx.append(t)
     freqs = {}
-for word in words:
-    if word not in freqs:
-        freqs[word] = 1
-    else:
-        freqs[word] += 1
+    for word in words:
+        if word not in freqs:
+            freqs[word] = 1
+        else:
+            freqs[word] += 1
 
-    keys = freqs.keys()  # word
-    values = freqs.values() #frequency
+        keys = freqs.keys()  # word
+        values = freqs.values()  # frequency
 
-colm = ['Freq'] 
-for v in freqs:
-    df = pd.DataFrame(data=values, index=keys, columns=colm)
-    df2 = (df.loc[df['Freq'] == 1])
-    num=(len(df2)) # går galt når jeg appender til listen "number"
-    number.append(num) # Vil du kigge her?
-   
-c = ['Unique words in doc']
-big_df = pd.DataFrame(data=number, index=idx, columns=c) 
-big_df #Skulle gerne være forskellige værdier i kolonnen "unique"
+        colm = ['Freq']
+        for v in freqs:
+            df = pd.DataFrame(data=values, index=keys, columns=colm)
+            df2 = (df.loc[df['Freq'] == 1])
+            num = (len(df2))  # går galt når jeg appender til listen "number"
+            number.append(num)  # Vil du kigge her?
+
+            c = ['Unique words in doc']
+            big_df = pd.DataFrame(data=num, index=idx, columns=c)
+            big_df  # Skulle gerne være forskellige værdier i kolonnen "unique"
 # Når dette sker skal den laves til csv og merges med Data.csv
 
-#big_df.to_csv(r'Data/Unique_ND.csv')
+# big_df.to_csv(r'Data/Unique_ND.csv')
 
 
-
-
-# %%
+# %% #PRINTER EN MASSE OG CHRASHER NÆSTEN INTERACTIVE WINDOW
 
 # defines text to be used
 your_file = open('Data/Final_UTF8_data/ND_data/ND_Tokenfolder/ND_token22.txt')
 text = your_file.read
 lines = data.split('/')
 
- words = []
-  ten_l_words = []
+words = []
+ten_l_words = []
 
-   # iterating through "lines" adding each separate word to the "words" array
-   for each in lines:
-        words += each.split(" ")
+# iterating through "lines" adding each separate word to the "words" array
+for each in lines:
+    words += each.split(" ")
 
     # checking to see if each word in the "words" array is 8 chars long, and if so
     # appending that words to the "eight_l_word" array
@@ -134,17 +132,16 @@ lines = data.split('/')
 
 # RIKKE LEGER HER
 # %%% #DET HER VIRKER IKKE
-import collections
 #path = glob.glob("Data/All_Tagged_Data/*.txt")
 path = glob.glob('Data/Final_UTF8_data/D_data/D_Tokenfolder/*.txt')
 word_length = []
 word_occurence = []
 row = []
 
-for fileName in path: 
-        data = open(fileName, "r").read()
-        words = data.split("/")
-        row.append(fileName)
+for fileName in path:
+    data = open(fileName, "r").read()
+    words = data.split("/")
+    row.append(fileName)
 for word in words:
     length = [len(word)]
     word_length.append(length)
@@ -169,7 +166,6 @@ for fileName in glob.iglob(r'Data/All_Tagged_Data/*.txt'):
         main()
 
 
-
 #####
 # %% #DET HER VIRKER PÅ 1 FIL
 def main():
@@ -180,6 +176,7 @@ def main():
     print('length | How often a word of this length occurs')
     for i in sorted(counts.keys()):
         print('%-6d | %d' % (i, counts[i]))
+
 
 if __name__ == '__main__':
     main()
