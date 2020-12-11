436181b1f9e134a411d815244cd57714ddbe3427
RikkeKjems <31722239+RikkeKjems@users.noreply.github.com>
2020-12-11 10:28:15 +0100

    logreg2

--- LogRegClass2.py
+++ LogRegClass2.py
@@ -1,8 +1,11 @@
 ##### IMPORTING / INSTALLING LIBRARIES ####
-#%%
+# %%
 # Packages / libraries
-import os #provides functions for interacting with the operating system
-import numpy as np 
+from sklearn.dummy import DummyClassifier
+from sklearn.model_selection import KFold
+from sklearn.linear_model import LogisticRegressionCV
+import os  # provides functions for interacting with the operating system
+import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
@@ -16,22 +19,23 @@ from math import sqrt
 %matplotlib inline
 
 # To change scientific numbers to float
-np.set_printoptions(formatter={'float_kind':'{:f}'.format})
+np.set_printoptions(formatter={'float_kind': '{:f}'.format})
 
 # Increases the size of sns plots
-sns.set(rc={'figure.figsize':(12,10)})
+sns.set(rc={'figure.figsize': (12, 10)})
 
 ### LOADING THE RAW DATA ###
-#%%
+# %%
 # Loading the data
-raw_data = pd.read_csv('Data/CSV/All.csv')
+raw_data = pd.read_csv('Data/CSV/F.csv')
 
 # print the shape
 print(raw_data.shape)
 
-#runs the first 5 rows
+# runs the first 5 rows
 raw_data.head(5)
 
+# %%
 #### DATA PREPROCCESSING #####
 #### EXPLORATORY DATA ANALYSIS ####
 """ 
@@ -55,19 +59,20 @@ print(raw_data.shape)
 raw_data.isnull().sum()
 """
 
-#%%
-# Investigate all the elements whithin each Feature 
+# %%
+# Investigate all the elements whithin each Feature
 
 for column in raw_data:
     unique_values = np.unique(raw_data[column])
     nr_values = len(unique_values)
     if nr_values <= 10:
-        print("The number of values for feature {} is: {} -- {}".format(column, nr_values, unique_values))
+        print("The number of values for feature {} is: {} -- {}".format(column,
+                                                                        nr_values, unique_values))
     else:
         print("The number of values for feature {} is: {}".format(column, nr_values))
 
 
-#%%
+# %%
 # Visualize the data using seaborn Pairplots
 
 g = sns.pairplot(raw_data)
@@ -76,43 +81,43 @@ g = sns.pairplot(raw_data)
 
 
 #### DATA CLEANING ####
-#%%
+# %%
 # Deleting the outlier
 
 raw_data = raw_data[raw_data['Age'] < 100]
 
 raw_data.shape
 
-#%%
+# %%
 # Visualize the data using seaborn Pairplots
-g = sns.pairplot(raw_data, hue = 'Good Loan')
+g = sns.pairplot(raw_data, hue='Good Loan')
 
-#%%
+# %%
 # Investigating the distr of y
-sns.countplot(x = 'Good Loan', data = raw_data, palette = 'Set3')
+sns.countplot(x='Good Loan', data=raw_data, palette='Set3')
 
-#%%
+# %%
 raw_data.columns
 
-#%%
+# %%
 # Looping through all the features by our y variable - see if there is relationship
 
 features = ['Type of Account', 'Account History', 'Reason for the Loan',
-       'Account Savings', 'Employment History',
-       'Individual Stauts', 'Other Loans', 'Security / Collateral',
-       'Residence Status', 'Job', 'Completed Other loan?']
+            'Account Savings', 'Employment History',
+            'Individual Stauts', 'Other Loans', 'Security / Collateral',
+            'Residence Status', 'Job', 'Completed Other loan?']
 
 for f in features:
-    sns.countplot(x = f, data = raw_data, palette = 'Set3', hue = 'Good Loan')
+    sns.countplot(x=f, data=raw_data, palette='Set3', hue='Good Loan')
     plt.show()
 
-#%%
+# %%
 raw_data.head()
 
-#%%
+# %%
 # Making categorical variables into numeric representation
 
-new_raw_data = pd.get_dummies(raw_data, columns = features)
+new_raw_data = pd.get_dummies(raw_data, columns=features)
 
 # Notes:
 # We can also do this with Label Encoding and OneHotEncoder from the preprocessing library
@@ -130,21 +135,21 @@ new_raw_data['Good Loan'][new_raw_data['Good Loan'] == 'No'] = 0
 new_raw_data
 
 # Notes:
-#We do not need to normalize / standardize the data in Logistic Regression due to the logistic function (0 or 1)
-#Once a value crosses the decision boundary (0.5 threshold), it saturates
-#After the 0.5 or before, there is no additional value to be added from smaller or larger values 
+# We do not need to normalize / standardize the data in Logistic Regression due to the logistic function (0 or 1)
+# Once a value crosses the decision boundary (0.5 threshold), it saturates
+# After the 0.5 or before, there is no additional value to be added from smaller or larger values
 
 #### FEATURE SELECTION ###
-#In this example, we do not have many variables so we might use all of the data but in some cases, you have thousands of variables and you will need to filter them in order to save computational time
-#Steps of Running Feature Importance:
-#-Split the data into X & y
-#-Run a Tree-based estimators (i.e. decision trees & random forests)
-#-Run Feature Importance
+# In this example, we do not have many variables so we might use all of the data but in some cases, you have thousands of variables and you will need to filter them in order to save computational time
+# Steps of Running Feature Importance:
+# -Split the data into X & y
+# -Run a Tree-based estimators (i.e. decision trees & random forests)
+# -Run Feature Importance
 
-#%%
+# %%
 # Split the data into X & y
 
-X = new_raw_data.drop('Good Loan', axis = 1).values
+X = new_raw_data.drop('Good Loan', axis=1).values
 y = new_raw_data['Good Loan']
 
 y = y.astype(int)
@@ -152,47 +157,48 @@ y = y.astype(int)
 print(X.shape)
 print(y.shape)
 
-#%%
+# %%
 # Run a Tree-based estimators (i.e. decision trees & random forests)
 
-dt = DecisionTreeClassifier(random_state=15, criterion = 'entropy', max_depth = 10)
-dt.fit(X,y)
+dt = DecisionTreeClassifier(random_state=15, criterion='entropy', max_depth=10)
+dt.fit(X, y)
 
 # If you want to learn how Decesion Trees work, read here: https://www.datacamp.com/community/tutorials/decision-tree-classification-python
 # Official Doc: https://scikit-learn.org/stable/modules/generated/sklearn.tree.DecisionTreeClassifier.html
 
-#%%
+# %%
 # Running Feature Importance
 
 fi_col = []
 fi = []
 
-for i,column in enumerate(new_raw_data.drop('Good Loan', axis = 1)):
-    print('The feature importance for {} is : {}'.format(column, dt.feature_importances_[i]))
-    
+for i, column in enumerate(new_raw_data.drop('Good Loan', axis=1)):
+    print('The feature importance for {} is : {}'.format(
+        column, dt.feature_importances_[i]))
+
     fi_col.append(column)
     fi.append(dt.feature_importances_[i])
 
-#%%
+# %%
 # Creating a Dataframe
 fi_col
 fi
 
 fi_df = zip(fi_col, fi)
-fi_df = pd.DataFrame(fi_df, columns = ['Feature','Feature Importance'])
+fi_df = pd.DataFrame(fi_df, columns=['Feature', 'Feature Importance'])
 fi_df
 
 
 # Ordering the data
-fi_df = fi_df.sort_values('Feature Importance', ascending = False).reset_index()
+fi_df = fi_df.sort_values('Feature Importance', ascending=False).reset_index()
 
 # Creating columns to keep
 columns_to_keep = fi_df['Feature'][0:40]
 
 fi_df
 
-##### SPLITTING THE RAW DATA - HOLD-OUT VALIDATION
-#%%
+# SPLITTING THE RAW DATA - HOLD-OUT VALIDATION
+# %%
 # Print the shapes
 
 print(new_raw_data.shape)
@@ -200,7 +206,7 @@ print(new_raw_data[columns_to_keep].shape)
 
 # new_raw_data = new_raw_data[columns_to_keep]
 
-#%%
+# %%
 # Split the data into X & y
 
 X = new_raw_data[columns_to_keep].values
@@ -213,14 +219,16 @@ y
 print(X.shape)
 print(y.shape)
 
-#%%
+# %%
 # Hold-out validation
 
 # first one
-X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.8, test_size=0.2, random_state=15)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, train_size=0.8, test_size=0.2, random_state=15)
 
 # Second one
-X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, train_size = 0.9, test_size=0.1, random_state=15)
+X_train, X_valid, y_train, y_valid = train_test_split(
+    X_train, y_train, train_size=0.9, test_size=0.1, random_state=15)
 
 print(X_train.shape)
 print(X_test.shape)
@@ -232,22 +240,22 @@ print(y_valid.shape)
 
 # Official Doc: https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html
 
-#%%
+# %%
 # Investigating the distr of all ys
 
-ax = sns.countplot(x = y_valid, palette = "Set3")
+ax = sns.countplot(x=y_valid, palette="Set3")
 
 
 #### RUNNING LOGISTIC REGRESSION ####
-#%%
+# %%
 # Training my model
 
-log_reg = LogisticRegression(random_state=10, solver = 'lbfgs')
+log_reg = LogisticRegression(random_state=10, solver='lbfgs')
 
 log_reg.fit(X_train, y_train)
 
 # SKLearn doc: https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegression.html
-#%%
+# %%
 # Methods we can use in Logistic
 
 # predict - Predict class labels for samples in X
@@ -264,7 +272,7 @@ log_reg.coef_
 
 
 #### EVALUATING THE MODEL ####
-#%%
+# %%
 # Accuracy on Train
 print("The Training Accuracy is: ", log_reg.score(X_train, y_train))
 
@@ -275,44 +283,48 @@ print("The Testing Accuracy is: ", log_reg.score(X_test, y_test))
 # Classification Report
 print(classification_report(y_train, y_pred))
 
-#%%
+# %%
 # Confusion Matrix function
+
 
 def plot_confusion_matrix(cm, classes=None, title='Confusion matrix'):
     """Plots a confusion matrix."""
     if classes is not None:
-        sns.heatmap(cm, cmap="YlGnBu", xticklabels=classes, yticklabels=classes, vmin=0., vmax=1., annot=True, annot_kws={'size':50})
+        sns.heatmap(cm, cmap="YlGnBu", xticklabels=classes, yticklabels=classes,
+                    vmin=0., vmax=1., annot=True, annot_kws={'size': 50})
     else:
         sns.heatmap(cm, vmin=0., vmax=1.)
     plt.title(title)
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
 
-#%%
+# %%
 # Visualizing cm
 
+
 cm = confusion_matrix(y_train, y_pred)
-cm_norm = cm / cm.sum(axis=1).reshape(-1,1)
+cm_norm = cm / cm.sum(axis=1).reshape(-1, 1)
 
-plot_confusion_matrix(cm_norm, classes = log_reg.classes_, title='Confusion matrix')
+plot_confusion_matrix(cm_norm, classes=log_reg.classes_,
+                      title='Confusion matrix')
 
-#%%
+# %%
 log_reg.classes_
 
-#%%
+# %%
 cm.sum(axis=1)
 cm_norm
 
-#%%
+# %%
 cm
 
-#%%
+# %%
 cm.sum(axis=0)
 
-#%%
+# %%
 np.diag(cm)
 
-#%%
+# %%
 # Calculating False Positives (FP), False Negatives (FN), True Positives (TP) & True Negatives (TN)
 
 FP = cm.sum(axis=0) - np.diag(cm)
@@ -339,8 +351,7 @@ FNR = FN / (FN + TP)
 print("The False Negative Rate is: ", FNR)
 
 
-
-##Total averages :
+# Total averages :
 print("")
 print("The average TPR is:", TPR.sum()/2)
 print("The average Precision is:", PPV.sum()/2)
@@ -348,13 +359,13 @@ print("The average False positive rate is:", FPR.sum()/2)
 print("The average False Negative Rate is:", FNR.sum()/2)
 
 # Logarithmic loss - or Log Loss - or cross-entropy loss
-#Log Loss is an error metric
-#This is the loss function used in (multinomial) logistic regression and extensions of it such as neural networks, defined as the negative log-likelihood of the true labels given a probabilistic classifier’s predictions.
-#Why it's important? For example, imagine having 2 models / classifiers that both predict one observation correctly (Good Loan). However, 1 classifier has a predicted probability of 0.54 and the other 0.95. Which one will you choose? Classification Accuracy will not help here as it will get both on 100%
-#Doc: https://scikit-learn.org/stable/modules/generated/sklearn.metrics.log_loss.html
+# Log Loss is an error metric
+# This is the loss function used in (multinomial) logistic regression and extensions of it such as neural networks, defined as the negative log-likelihood of the true labels given a probabilistic classifier’s predictions.
+# Why it's important? For example, imagine having 2 models / classifiers that both predict one observation correctly (Good Loan). However, 1 classifier has a predicted probability of 0.54 and the other 0.95. Which one will you choose? Classification Accuracy will not help here as it will get both on 100%
+# Doc: https://scikit-learn.org/stable/modules/generated/sklearn.metrics.log_loss.html
 
 ######
-#%%
+# %%
 # Running Log loss on training
 print("The Log Loss on Training is: ", log_loss(y_train, pred_proba))
 
@@ -364,23 +375,24 @@ print("The Log Loss on Testing Dataset is: ", log_loss(y_test, pred_proba_t))
 
 
 ##### HYPER PARAMETER TUNING ###
-#We will loop over parameter C (Inverse of regularization strength).
-#Inverse of regularization strength helps to avoid overfitting - it penalizes large values of your parameters
-#It also helps to find Global Minimum by moving to better "solutions" from local minimum to global minimum
-#The values of C to search should be n-equally-spaced values in log space ranging from 1e-5 to 1e5
-#%%
+# We will loop over parameter C (Inverse of regularization strength).
+# Inverse of regularization strength helps to avoid overfitting - it penalizes large values of your parameters
+# It also helps to find Global Minimum by moving to better "solutions" from local minimum to global minimum
+# The values of C to search should be n-equally-spaced values in log space ranging from 1e-5 to 1e5
+# %%
 np.geomspace(1e-5, 1e5, num=20)
 
-#%%
+# %%
 # Creating a range for C values
 np.geomspace(1e-5, 1e5, num=20)
 
 # ploting it
-plt.plot(np.geomspace(1e-5, 1e5, num=20)) #  uniformly distributed in log space
-plt.plot(np.linspace(1e-5, 1e5, num=20)) # uniformly distributed in linear space, instead of log space
+plt.plot(np.geomspace(1e-5, 1e5, num=20))  # uniformly distributed in log space
+# uniformly distributed in linear space, instead of log space
+plt.plot(np.linspace(1e-5, 1e5, num=20))
 # plt.plot(np.logspace(np.log10(1e-5) , np.log10(1e5) , num=20)) # same as geomspace
 
-#%%
+# %%
 # Looping over the parameters
 
 C_List = np.geomspace(1e-5, 1e5, num=20)
@@ -388,7 +400,7 @@ CA = []
 Logarithmic_Loss = []
 
 for c in C_List:
-    log_reg2 = LogisticRegression(random_state=10, solver = 'lbfgs', C=c)
+    log_reg2 = LogisticRegression(random_state=10, solver='lbfgs', C=c)
     log_reg2.fit(X_train, y_train)
     score = log_reg2.score(X_test, y_test)
     CA.append(score)
@@ -399,7 +411,7 @@ for c in C_List:
     print("The Logg Loss of C parameter {} is {}:".format(c, log_loss2))
     print("")
 
-#%%
+# %%
 # putting the outcomes in a Table
 
 # reshaping
@@ -409,26 +421,25 @@ Logarithmic_Loss2 = np.array(Logarithmic_Loss).reshape(20,)
 # zip
 outcomes = zip(C_List, CA2, Logarithmic_Loss2)
 
-#df
-df_outcomes = pd.DataFrame(outcomes, columns = ["C_List", 'CA2','Logarithmic_Loss2'])
+# df
+df_outcomes = pd.DataFrame(
+    outcomes, columns=["C_List", 'CA2', 'Logarithmic_Loss2'])
 
-#print
+# print
 df_outcomes
 
 # Ordering the data (sort_values)
-df_outcomes.sort_values("Logarithmic_Loss2", ascending = True).reset_index()
+df_outcomes.sort_values("Logarithmic_Loss2", ascending=True).reset_index()
 
-#%%
+# %%
 # Another way of doing the above
-# Scikit-learn offers a LogisticRegressionCV module which implements Logistic Regression 
+# Scikit-learn offers a LogisticRegressionCV module which implements Logistic Regression
 # with builtin cross-validation to find out the optimal C parameter
 
-from sklearn.linear_model import LogisticRegressionCV
-from sklearn.model_selection import KFold
 kf = KFold(n_splits=3, random_state=0, shuffle=True)
 
 # Logistic Reg CV
-Log_reg3 = LogisticRegressionCV(random_state=15, Cs = C_List, solver ='lbfgs')
+Log_reg3 = LogisticRegressionCV(random_state=15, Cs=C_List, solver='lbfgs')
 Log_reg3.fit(X_train, y_train)
 print("The CA is:", Log_reg3.score(X_test, y_test))
 pred_proba_t = Log_reg3.predict_proba(X_test)
@@ -436,7 +447,6 @@ log_loss3 = log_loss(y_test, pred_proba_t)
 print("The Logistic Loss is: ", log_loss3)
 
 print("The optimal C parameter is: ", Log_reg3.C_)
-
 
 
 # Doc: https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegressionCV.html
@@ -453,7 +463,7 @@ Hold-out:
 Advantage: The advantage of Hold-out is that you can test how your model performs on completely unseen data that you haven't used when training the model. Additionally, Hold-out is usually much faster and less computationally expensive. Disadvantage: The evaluation may depend heavily on which data points end up in the training set and which end up in the test set, and thus the evaluation may be significantly different depending on how the division is made.
 """
 
-#%%
+# %%
 # Maybe we have a different metric we want to track
 
 # Looping over the parameters
@@ -463,7 +473,7 @@ CA = []
 Logarithmic_Loss = []
 
 for c in C_List:
-    log_reg2 = LogisticRegression(random_state=10, solver = 'lbfgs', C=c)
+    log_reg2 = LogisticRegression(random_state=10, solver='lbfgs', C=c)
     log_reg2.fit(X_train, y_train)
     score = log_reg2.score(X_test, y_test)
     CA.append(score)
@@ -473,17 +483,17 @@ for c in C_List:
     Logarithmic_Loss.append(log_loss2)
     print("The Logg Loss of C parameter {} is {}:".format(c, log_loss2))
     print("")
-    
+
     y_pred = log_reg2.predict(X_train)
     cm = confusion_matrix(y_train, y_pred)
-    cm_norm = cm / cm.sum(axis=1).reshape(-1,1)
-    plot_confusion_matrix(cm_norm, classes = log_reg.classes_, title='Confusion matrix')
+    cm_norm = cm / cm.sum(axis=1).reshape(-1, 1)
+    plot_confusion_matrix(cm_norm, classes=log_reg.classes_,
+                          title='Confusion matrix')
     plt.show()
 
-#%%
+# %%
 # Training a Dummy Classifier
 
-from sklearn.dummy import DummyClassifier
 
 dummy_clf = DummyClassifier(strategy="most_frequent")
 dummy_clf.fit(X_train, y_train)
@@ -499,11 +509,11 @@ print("Log Loss:", log_loss2)
 # Doc: https://scikit-learn.org/stable/modules/generated/sklearn.dummy.DummyClassifier.html
 
 
-#FINAL MODEL WITH SELECTED PARAMETERS
-#%%
-# Final Model 
+# FINAL MODEL WITH SELECTED PARAMETERS
+# %%
+# Final Model
 
-log_reg3 = LogisticRegression(random_state=10, solver = 'lbfgs', C=784.759970)
+log_reg3 = LogisticRegression(random_state=10, solver='lbfgs', C=784.759970)
 log_reg3.fit(X_train, y_train)
 score = log_reg3.score(X_valid, y_valid)
 
@@ -513,4 +523,4 @@ log_loss2 = log_loss(y_valid, pred_proba_t)
 print("Testing Acc:", score)
 print("Log Loss:", log_loss2)
 
-#%%
+# %%
